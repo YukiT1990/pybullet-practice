a7feb7309baf752e937a2f12408d066566061f5c
YukiT1990 <07mikasanoyamani.idesitukikamo@gmail.com>
2021-10-23 13:52:36 +0900

    add test.py

---
+++ week3/test.py
@@ -0,0 +1,14 @@
+import unittest
+import numpy as np
+
+class MyTest (unittest.TestCase):
+
+  def test_mean(self):
+    mean = np.mean([1, 2, 3])
+    self.assertEqual(mean, 2)
+
+  def test_mean2(self):
+    mean = np.mean([1, 1, 1])
+    self.assertEqual(mean, 2)
+
+unittest.main()
