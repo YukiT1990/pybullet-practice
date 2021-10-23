afecd6b6050c1928517a13e8aa18f9b457ca200c
YukiT1990 <07mikasanoyamani.idesitukikamo@gmail.com>
2021-10-23 15:30:33 +0900

    3.204 Genome spec

--- week3/genome.py
+++ week3/genome.py
@@ -1,4 +1,5 @@
 # 3.203 Generate a genome
+# 3.204 Genome spec
 
 import numpy as np
 
@@ -15,4 +16,28 @@ class Genome():
 
   @staticmethod
   def get_gene_spec():
-    return {}
+    gene_spec = {
+      "link-shape": {"scale": 1},
+      "link-length": {"scale": 1},
+      "link-radius": {"scale": 1},
+      "link-recurrance": {"scale": 4},
+      "link-mass": {"scale": 4},
+      "joint-type": {"scale": 1},
+      "joint-parent": {"scale": 1},
+      "joint-axis-xyz": {"scale": 1},
+      "joint-origin-rpy-1": {"scale": np.pi * 2},
+      "joint-origin-rpy-2": {"scale": np.pi * 2},
+      "joint-origin-rpy-3": {"scale": np.pi * 2},
+      "joint-origin-xyz-1": {"scale": 1},
+      "joint-origin-xyz-2": {"scale": 1},
+      "joint-origin-xyz-3": {"scale": 1},
+      "control-waveform": {"scale": 1},
+      "control-amp": {"scale": 0.25},
+      "control-freq": {"scale": 1},
+      }
+    ind = 0
+    for key in gene_spec.keys():
+      gene_spec[key]["ind"] = ind
+      ind = ind + 1
+    return gene_spec
+
--- week3/test_genome.py
+++ week3/test_genome.py
@@ -1,4 +1,5 @@
 # 3.203 Generate a genome
+# 3.204 Genome spec
 
 import unittest
 import genome
@@ -17,7 +18,7 @@ class GenomeTest (unittest.TestCase):
 
   def testRandomGeneHasValues(self):
     gene = genome.Genome.get_random_gene(5)
-    print(gene)
+    # print(gene)
     self.assertIsNotNone(gene[0])
 
   def testRandomGeneLength(self):
@@ -30,11 +31,25 @@ class GenomeTest (unittest.TestCase):
 
   def testRandomGeneExists(self):
     data = genome.Genome.get_random_genome(20, 5)
-    print(data)
+    # print(data)
     self.assertIsNotNone(data)
 
   def testGeneSpecExists(self):
     spec = genome.Genome.get_gene_spec()
     self.assertIsNotNone(spec)
+
+  def testGeneSpecHasLinkLength(self):
+    spec = genome.Genome.get_gene_spec()
+    self.assertIsNotNone(spec['link-length'])
+
+  def testGeneSpecHasLinkLength(self):
+    spec = genome.Genome.get_gene_spec()
+    print(spec)
+    self.assertIsNotNone(spec['link-length']["ind"])
+
+  def testGeneSpecScale(self):
+    spec = genome.Genome.get_gene_spec()
+    gene = genome.Genome.get_random_gene(20)
+    self.assertGreater(gene[spec["link-length"]["ind"]], 0)
 
 unittest.main()
