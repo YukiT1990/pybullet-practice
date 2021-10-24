4262ba37fdaf51fdc3d0e388096cda546c47896f
YukiT1990 <07mikasanoyamani.idesitukikamo@gmail.com>
2021-10-24 13:54:13 +0900

    stop copying code from 3.215 toXML

--- week3/test_genome.py
+++ week3/test_genome.py
@@ -7,93 +7,100 @@ import unittest
 import genome
 import numpy as np
 
+
 class GenomeTest (unittest.TestCase):
 
-  def testClassExists(self):
-    self.assertIsNotNone(genome.Genome)
+    def testClassExists(self):
+        self.assertIsNotNone(genome.Genome)
 
-  def testRandomGene(self):
-    self.assertIsNotNone(genome.Genome.get_random_gene)
+    def testRandomGene(self):
+        self.assertIsNotNone(genome.Genome.get_random_gene)
 
-  def testRandomGeneNotNone(self):
-    self.assertIsNotNone(genome.Genome.get_random_gene(5))
+    def testRandomGeneNotNone(self):
+        self.assertIsNotNone(genome.Genome.get_random_gene(5))
 
-  def testRandomGeneHasValues(self):
-    gene = genome.Genome.get_random_gene(5)
-    # print(gene)
-    self.assertIsNotNone(gene[0])
+    def testRandomGeneHasValues(self):
+        gene = genome.Genome.get_random_gene(5)
+        # print(gene)
+        self.assertIsNotNone(gene[0])
 
-  def testRandomGeneLength(self):
-    gene = genome.Genome.get_random_gene(20)
-    self.assertEqual(len(gene), 20)
+    def testRandomGeneLength(self):
+        gene = genome.Genome.get_random_gene(20)
+        self.assertEqual(len(gene), 20)
 
-  def testRandomGeneIsNumpyArrays(self):
-    gene = genome.Genome.get_random_gene(20)
-    self.assertEqual(type(gene), np.ndarray)
+    def testRandomGeneIsNumpyArrays(self):
+        gene = genome.Genome.get_random_gene(20)
+        self.assertEqual(type(gene), np.ndarray)
 
-  def testRandomGeneExists(self):
-    data = genome.Genome.get_random_genome(20, 5)
-    # print(data)
-    self.assertIsNotNone(data)
+    def testRandomGeneExists(self):
+        data = genome.Genome.get_random_genome(20, 5)
+        # print(data)
+        self.assertIsNotNone(data)
 
-  def testGeneSpecExists(self):
-    spec = genome.Genome.get_gene_spec()
-    self.assertIsNotNone(spec)
+    def testGeneSpecExists(self):
+        spec = genome.Genome.get_gene_spec()
+        self.assertIsNotNone(spec)
 
-  def testGeneSpecHasLinkLength(self):
-    spec = genome.Genome.get_gene_spec()
-    self.assertIsNotNone(spec['link-length'])
+    def testGeneSpecHasLinkLength(self):
+        spec = genome.Genome.get_gene_spec()
+        self.assertIsNotNone(spec['link-length'])
 
-  def testGeneSpecHasLinkLength(self):
-    spec = genome.Genome.get_gene_spec()
-    # print(spec)
-    self.assertIsNotNone(spec['link-length']["ind"])
+    def testGeneSpecHasLinkLength(self):
+        spec = genome.Genome.get_gene_spec()
+        # print(spec)
+        self.assertIsNotNone(spec['link-length']["ind"])
 
-  def testGeneSpecScale(self):
-    spec = genome.Genome.get_gene_spec()
-    gene = genome.Genome.get_random_gene(20)
-    self.assertGreater(gene[spec["link-length"]["ind"]], 0)
+    def testGeneSpecScale(self):
+        spec = genome.Genome.get_gene_spec()
+        gene = genome.Genome.get_random_gene(20)
+        self.assertGreater(gene[spec["link-length"]["ind"]], 0)
 
-  def testFlatLinks(self):
-    links = [
-      genome.URDFLink(name="A", parent_name=None, recur=1),
-      genome.URDFLink(name="B", parent_name="A", recur=1),
-      genome.URDFLink(name="C", parent_name="B", recur=1),
-      genome.URDFLink(name="D", parent_name="C", recur=1)
-    ]
-    self.assertIsNotNone(links)
+    def testFlatLinks(self):
+        links = [
+            genome.URDFLink(name="A", parent_name=None, recur=1),
+            genome.URDFLink(name="B", parent_name="A", recur=1),
+            genome.URDFLink(name="C", parent_name="B", recur=1),
+            genome.URDFLink(name="D", parent_name="C", recur=1)
+        ]
+        self.assertIsNotNone(links)
 
-  def testExpandLinks(self):
-    links = [
-      genome.URDFLink(name="A", parent_name="None", recur=1),
-      genome.URDFLink(name="B", parent_name="A", recur=1),
-      genome.URDFLink(name="C", parent_name="B", recur=2),
-      genome.URDFLink(name="D", parent_name="C", recur=1)
-    ]
-    exp_links = [links[0]]
-    genome.Genome.expandLinks(links[0], links[0].name, links, exp_links)
-    names = [l.name+"-parent-is-"+l.parent_name for l in exp_links]
-    print(names)
-    self.assertEqual(len(exp_links), 6)
+    def testExpandLinks(self):
+        links = [
+            genome.URDFLink(name="A", parent_name="None", recur=1),
+            genome.URDFLink(name="B", parent_name="A", recur=1),
+            genome.URDFLink(name="C", parent_name="B", recur=2),
+            genome.URDFLink(name="D", parent_name="C", recur=1)
+        ]
+        exp_links = [links[0]]
+        genome.Genome.expandLinks(links[0], links[0].name, links, exp_links)
+        names = [l.name+"-parent-is-"+l.parent_name for l in exp_links]
+        print(names)
+        self.assertEqual(len(exp_links), 6)
 
-  def testGeneToGeneDict(self):
-    spec = genome.Genome.get_gene_spec()
-    gene = genome.Genome.get_random_gene(len(spec))
-    gene_dict = genome.Genome.get_gene_dict(gene, spec)
-    self.assertIn("link-recurrance", gene_dict)
+    def testGeneToGeneDict(self):
+        spec = genome.Genome.get_gene_spec()
+        gene = genome.Genome.get_random_gene(len(spec))
+        gene_dict = genome.Genome.get_gene_dict(gene, spec)
+        self.assertIn("link-recurrance", gene_dict)
 
-  def testGeneToDict(self):
-    spec = genome.Genome.get_gene_spec()
-    dna = genome.Genome.get_random_genome(len(spec), 3)
-    genome_dicts = genome.Genome.get_genome_dicts(dna, spec)
-    self.assertEqual(len(genome_dicts), 3)
+    def testGeneToDict(self):
+        spec = genome.Genome.get_gene_spec()
+        dna = genome.Genome.get_random_genome(len(spec), 3)
+        genome_dicts = genome.Genome.get_genome_dicts(dna, spec)
+        self.assertEqual(len(genome_dicts), 3)
 
-  def testGetLinks(self):
-    spec = genome.Genome.get_gene_spec()
-    dna = genome.Genome.get_random_genome(len(spec), 3)
-    genome_dicts = genome.Genome.get_genome_dicts(dna, spec)
-    links = genome.Genome.genome_to_links(genome_dicts)
-    self.assertEqual(len(links), 3)
+    def testGetLinks(self):
+        spec = genome.Genome.get_gene_spec()
+        dna = genome.Genome.get_random_genome(len(spec), 3)
+        genome_dicts = genome.Genome.get_genome_dicts(dna, spec)
+        links = genome.Genome.genome_to_links(genome_dicts)
+        self.assertEqual(len(links), 3)
+
+# 3.215 toXML
+    # def testLinkToXML(self):
+    #     link = genome.URDFLink(nmae="A", parent_name="None", recur=1)
+    #     xml_str = link.to_link_xml()
+    #     self.assertIsNotNone(xml_str)
 
 
 unittest.main()
